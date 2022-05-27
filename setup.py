37b9e2dbea181e715dec13a9a2a7bf6a4bf3c369
Marios S. Kyriakou <mkiria01@ucy.ac.cy>
2022-05-27 17:14:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name=module_name,
-    version="0.0.1",
+    version="0.0.2",
     author="KIOS CoE developers",
     author_email="kiriakou.marios@ucy.ac.cy",
     description=f"The {module_name} is inspired by the EPANET-Matlab Toolkit.",
