09b715920b3100e148bb8730f9b307d3bc2d46b9
Marios S. Kyriakou <mkiria01@ucy.ac.cy>
2022-08-30 11:51:48 +0300

    Update testFunctions_2.py

--- epyt/tests/testFunctions_2.py
+++ epyt/tests/testFunctions_2.py
@@ -7,7 +7,6 @@ Step-by-step execution. You may also use the breakpoints.
 import matplotlib.pyplot as plt
 from epyt import epanet
 
-
 # Create EPANET object using the INP file
 d = epanet('Net1.inp')  # Net1 Net2 Net3 BWSN_Network_1
 
@@ -20,8 +19,8 @@ if d.getLinkPumpCount():
     d.setLinkPumpHeadCurveIndex(index, indexCurve)
     [HeadCurveIndex, PumpIndex] = d.getLinkPumpHeadCurveIndex()
     d.deleteLink(index)
- 
-#%% Add/delete Node - 
+
+# %% Add/delete Node -
 # Select the position of the node to be added/deleted 
 # by clicking on the plot
 d.plot(nodesID=True)
@@ -29,13 +28,13 @@ print('\nAdd a junction')
 d.printv(d.getNodeNameID())
 d.printv(d.getNodeJunctionCount())
 xy = plt.ginput(1)[0]
-index = d.addNodeJunction('Junction')    
+index = d.addNodeJunction('Junction')
 d.setNodeCoordinates(index, [xy[0], xy[1]])
 d.printv(d.getNodeNameID())
 d.printv(d.getNodeJunctionCount())
 d.plot(nodesID=True)
 
-#%%
+# %%
 print('\nAdd a reservoir')
 d.getNodeReservoirCount()
 xy = plt.ginput(1)[0]
@@ -45,7 +44,7 @@ d.printv(d.getNodeNameID())
 d.printv(d.getNodeReservoirCount())
 d.plot(nodesID=True)
 
-#%%
+# %%
 print('\nAdd a tank')
 d.getNodeTankCount()
 xy = plt.ginput(1)[0]
@@ -55,7 +54,7 @@ d.printv(d.getNodeNameID())
 d.printv(d.getNodeTankCount())
 d.plot(nodesID=True)
 
-#%%
+# %%
 print('\nDelete a node')
 d.getNodeCount()
 d.deleteNode('Junction')
@@ -63,7 +62,7 @@ d.printv(d.getNodeNameID())
 d.printv(d.getNodeCount())
 d.plot(nodesID=True)
 
-#%% Add/delete Link
+# %% Add/delete Link
 print('\nAdd a cv pipe')
 d.printv(d.getLinkNameID())
 d.printv(d.getLinkPipeCount())
@@ -77,7 +76,7 @@ d.plot(linksID=True)
 d.deleteLink(index)
 d.plot(linksID=True)
 
-#%%
+# %%
 print('\nAdd a pipe')
 d.printv(d.getLinkNameID())
 d.printv(d.getLinkPipeCount())
@@ -88,7 +87,7 @@ d.printv(d.getLinkNameID())
 d.printv(d.getLinkPipeCount())
 d.deleteLink(index)
 
-#%%
+# %%
 print('\nAdd a pump')
 d.printv(d.getLinkPumpCount())
 fromNode = d.getNodeNameID(2)
@@ -99,21 +98,20 @@ d.printv(d.getLinkPumpCount())
 d.printv(d.getLinkType())
 d.deleteLink(index)
 
-#%%
-#similar
+# %%
+# similar
 print('\nAdd a valve')
-index = d.addLinkValvePRV('PRV-V', fromNode, toNode) 
-index1 = d.addLinkValveFCV('FCV', fromNode, toNode) 
-index2 = d.addLinkValveGPV('GPV', fromNode, toNode) 
-index3 = d.addLinkValvePBV('PBV', fromNode, toNode) 
-index4 = d.addLinkValvePSV('PSV', fromNode, toNode) 
-index5 = d.addLinkValveTCV('TCV', fromNode, toNode) 
+index = d.addLinkValvePRV('PRV-V', fromNode, toNode)
+index1 = d.addLinkValveFCV('FCV', fromNode, toNode)
+index2 = d.addLinkValveGPV('GPV', fromNode, toNode)
+index3 = d.addLinkValvePBV('PBV', fromNode, toNode)
+index4 = d.addLinkValvePSV('PSV', fromNode, toNode)
+index5 = d.addLinkValveTCV('TCV', fromNode, toNode)
 d.printv(d.getLinkType())
 
-#%%
+# %%
 # Unload library
 d.unload()
 
 # Close all figures
 # plt.close("all")
-
