420e66a796e02a5c585489db4471c2fc80349817
Marios S. Kyriakou <mariosmsk@gmail.com>
2024-04-17 16:44:34 +0300

    Update epanet.py

--- epyt/epanet.py
+++ epyt/epanet.py
@@ -10378,8 +10378,14 @@ class epanet:
             safe_delete(self.TempInpFile)
 
             cwd = os.getcwd()
-            tmp_files = list(filter(lambda f: os.path.isfile(os.path.join(cwd, f))
-                                      and (f.startswith("s") or f.startswith("en")) and 6 <= len(f) <= 8 and "." not in f))
+            files = os.listdir(cwd)
+            tmp_files = [
+                f for f in files
+                if os.path.isfile(os.path.join(cwd, f)) and
+                   (f.startswith('s') or f.startswith('en')) and
+                   6 <= len(f) <= 8 and
+                   "." not in f
+            ]
             tmp_files_paths = [os.path.join(cwd, f) for f in tmp_files]
             safe_delete(tmp_files_paths)
 
