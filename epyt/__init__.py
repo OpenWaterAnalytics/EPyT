4f4d1dc25e846d31eb40f7b12062d710aa86aade
Marios S. Kyriakou <mariosmsk@gmail.com>
2024-06-23 13:11:28 +0300

    update msx version

--- epyt/__init__.py
+++ epyt/__init__.py
@@ -2,7 +2,7 @@
 __author__ = """Marios S. Kyriakou"""
 __email__ = "kiriakou.marios@ucy.ac.cy"
 __version__ = "1.1.8"
-__msxversion__ = "2.0.0"
+__msxversion__ = "200000"
 __copyright__ = """Copyright 2022, KIOS Research and Innovation Center of Excellence (KIOS CoE), 
 University of Cyprus (www.kios.org.cy)."""
 __license__ = "EUPL License, Version 1.2"
