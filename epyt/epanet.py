3fd0919f73b7f64544b00a5f88f49b22ce75a87b
ichrys03 <79056704+ichrys03@users.noreply.github.com>
2024-09-03 16:10:43 +0300

    typepump index 3 added 2.3 update

--- epyt/epanet.py
+++ epyt/epanet.py
@@ -530,7 +530,7 @@ class epanet:
         # Node types
         self.TYPENODE = ['JUNCTION', 'RESERVOIR', 'TANK']
         # Constants for pumps
-        self.TYPEPUMP = ['CONSTANT_HORSEPOWER', 'POWER_FUNCTION', 'CUSTOM', '']
+        self.TYPEPUMP = ['CONSTANT_HORSEPOWER', 'POWER_FUNCTION', 'CUSTOM', 'NO_CURVE']
         # Link PUMP status
         self.TYPEPUMPSTATE = ['XHEAD', '', 'CLOSED', 'OPEN', '', 'XFLOW']
         # Constants for quality
