7ad1e1e77421e8f89588d3d63f228b7e47a86c6f
Marios S. Kyriakou <mariosmsk@gmail.com>
2025-05-10 01:58:30 +0300

    Update __init__.py

--- epyt/__init__.py
+++ epyt/__init__.py
@@ -3,7 +3,7 @@ __author__ = """Marios S. Kyriakou"""
 __email__ = "kiriakou.marios@ucy.ac.cy"
 __version__ = "1.2.2"
 __msxversion__ = "200000"
-__lastupdate__ = "08/05/2025"
+__lastupdate__ = "10/05/2025"
 __copyright__ = """Copyright 2022, KIOS Research and Innovation Center of Excellence (KIOS CoE), 
 University of Cyprus (www.kios.org.cy)."""
 __license__ = "EUPL License, Version 1.2"
