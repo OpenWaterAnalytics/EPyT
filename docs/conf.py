860d1bfe499eb319c707b5e2bfcf074760ee4a03
John Chrysostomou <chrysostomoujohn@gmail.com>
2026-02-25 14:00:35 +0200

    update on docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,18 @@
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+import os
 
+# Project root (so 'epyt' is importable as a package)
+sys.path.insert(0, os.path.abspath('..'))
+
+# epyt/src/ so modules inside src are discoverable by autodoc
+sys.path.insert(0, os.path.abspath('../epyt/src'))
 
 # -- Project information -----------------------------------------------------
 
 project = 'epyt'
 copyright = '2022, KIOS Research and Innovation Center of Excellence'
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here.
-import sys
-import os
-sys.path.insert(0, os.path.abspath('..'))
-
 author = 'KIOS CoE'
 
 # The full version, including alpha/beta/rc tags
@@ -26,9 +20,6 @@ release = '1.0.7'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -39,26 +30,25 @@ extensions = [
     'sphinx_copybutton'
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+# Mock C extensions that cannot be imported during doc builds
+autodoc_mock_imports = ['cffi']
+
+# Autodoc settings - automatically document all members
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+}
+
+autosummary_generate = True
+
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 html_theme = 'sphinx_rtd_theme'
-
-# extensions = ['sphinx.ext.autosummary',]
-# autodoc_default_flags = ['members']
-# autosummary_generate = True
+html_static_path = ['_static']
